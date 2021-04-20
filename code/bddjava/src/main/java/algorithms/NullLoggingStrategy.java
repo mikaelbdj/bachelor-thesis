26097c2acb7e7bcd58d533c6941fa43b036d3a7d
mikaelbdj <mikaelbdj@gmail.com>
2021-04-21 01:14:43 +0200

    Increase initial nodenum

---
+++ code/bddjava/src/main/java/algorithms/NullLoggingStrategy.java
@@ -0,0 +1,18 @@
+package algorithms;
+
+public class NullLoggingStrategy implements LoggingStrategy {
+    @Override
+    public void logSccFound() {
+
+    }
+
+    @Override
+    public void logStarted(String algName) {
+
+    }
+
+    @Override
+    public void logFinished(String algName) {
+
+    }
+}
---
+++ code/bddjava/src/main/java/algorithms/VerboseLoggingStrategy.java
@@ -0,0 +1,18 @@
+package algorithms;
+
+public class VerboseLoggingStrategy implements LoggingStrategy {
+    @Override
+    public void logSccFound() {
+
+    }
+
+    @Override
+    public void logStarted() {
+
+    }
+
+    @Override
+    public void logFinished() {
+
+    }
+}
