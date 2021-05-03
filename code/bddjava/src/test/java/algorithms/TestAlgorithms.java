0b816109d805f81823a5a24f6806da472cf1516c
magda-kc <magdalenakc@outlook.com>
2021-05-03 21:39:50 +0200

    add test for iterative linear

--- code/bddjava/src/test/java/algorithms/TestAlgorithms.java
+++ code/bddjava/src/test/java/algorithms/TestAlgorithms.java
@@ -200,4 +200,43 @@ public class TestAlgorithms {
         assertTrue(intSccs.contains(expectedScc8));
     }
 
+    @Test
+    public void testLinearIterativeShouldFindCorrectSCCs(){
+        algorithm = new LinearIterative(new NullLoggingStrategy());
+        Set<BDD> sccs = algorithm.run(bddGraph);
+
+        List<Set<Integer>> intSccs = sccs.stream().map(scc -> bddGraph.nodeSetToIntegerSet(scc)).collect(Collectors.toList());
+        Set<Integer> expectedScc1 = new HashSet<>();
+        expectedScc1.add(8);
+        Set<Integer> expectedScc2 = new HashSet<>();
+        expectedScc2.add(0);
+        expectedScc2.add(1);
+        Set<Integer> expectedScc3 = new HashSet<>();
+        expectedScc3.add(4);
+        Set<Integer> expectedScc4 = new HashSet<>();
+        expectedScc4.add(9);
+        Set<Integer> expectedScc5 = new HashSet<>();
+        expectedScc5.add(2);
+        Set<Integer> expectedScc6 = new HashSet<>();
+        expectedScc6.add(3);
+        expectedScc6.add(7);
+        expectedScc6.add(11);
+        Set<Integer> expectedScc7 = new HashSet<>();
+        expectedScc7.add(5);
+        expectedScc7.add(6);
+        expectedScc7.add(10);
+        Set<Integer> expectedScc8 = new HashSet<>();
+        expectedScc8.add(12);
+
+        assertEquals(8, intSccs.size());
+        assertTrue(intSccs.contains(expectedScc1));
+        assertTrue(intSccs.contains(expectedScc2));
+        assertTrue(intSccs.contains(expectedScc3));
+        assertTrue(intSccs.contains(expectedScc4));
+        assertTrue(intSccs.contains(expectedScc5));
+        assertTrue(intSccs.contains(expectedScc6));
+        assertTrue(intSccs.contains(expectedScc7));
+        assertTrue(intSccs.contains(expectedScc8));
+    }
+
 }
